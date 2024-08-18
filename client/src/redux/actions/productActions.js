f8b9d4be39ca5df8d5e448a8750433d9dfefee5a
Junlin He <hejunlin1992@gmail.com>
2024-08-17 18:45:16 -0700

    End of Star Rating

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,4 +1,88 @@
-import { setProducts, setLoading, setError, setPagination, setFavorites, setFavoritesToggle, setProduct } from "../slices/product";
+// import { setProducts, setLoading, setError, setPagination, setFavorites, setFavoritesToggle, setProduct } from "../slices/product";
+// import axios from 'axios';
+
+// export const getProducts = (page, favouriteToggle) => async (dispatch) => {
+//     dispatch(setLoading());
+//     try {
+//         const { data } = await axios.get(`/api/products/${page}/${10}`);
+//         const { products, pagination } = data;
+//         dispatch(setProducts(products));
+//         dispatch(setPagination(pagination));
+//     } catch (error) {
+//         dispatch(
+//             setError(
+//                 error.response && error.response.data.message
+//                     ? error.response.data.message
+//                     : error.message
+//                         ? error.message
+//                         : 'An expected error has occured.'
+//             )
+//         );
+//     }
+// };
+
+// export const addToFavorites = (id) => async (dispatch, getState) => {
+//     const {
+//         product: { favorites },
+//     } = getState();
+//     const newFavorites = [...favorites, id];
+//     localStorage.setItem('favorites', JSON.stringify(newFavorites));
+//     dispatch(setFavorites(newFavorites));
+// };
+
+// export const removeFromFavorites = (id) => async (dispatch, getState) => {
+//     const {
+//         product: { favorites },
+//     } = getState();
+//     const newFavorites = favorites.filter((favoriteId) => favoriteId !== id);
+//     localStorage.setItem('favorites', JSON.stringify(newFavorites));
+//     dispatch(setFavorites(newFavorites));
+// };
+
+// export const toggleFavorites = (toggle) => async (dispatch, getState) => {
+//     const {
+//         product: { favorites, products },
+//     } = getState();
+//     if (toggle) {
+//         const filteredProducts = products.filter((product) => favorites.includes(product._id));
+//         dispatch(setFavoritesToggle(true));
+//         dispatch(setProducts(filteredProducts));
+//     } else {
+//         dispatch(setFavoritesToggle(false));
+//         dispatch(getProducts(1));
+//     }
+// };
+
+// export const getProduct = (id) => async (dispatch) => {
+//     dispatch(setLoading(true))
+//     try {
+//         const { data } = await axios.get(`/api/products/${id}`)
+//         dispatch(setProduct(data));
+//     } catch (error) {
+//         dispatch(
+//             setError(
+//                 error.response && error.response.data.message
+//                     ? error.response.data.message
+//                     : error.message
+//                         ? error.message
+//                         : 'An expected error has occured.'
+//             )
+//         );
+//     }
+// }
+
+
+import {
+    setProducts,
+    setLoading,
+    setError,
+    setPagination,
+    setFavorites,
+    setFavoritesToggle,
+    setProduct,
+    productReviewed,
+    resetError,
+} from '../slices/product';
 import axios from 'axios';
 
 export const getProducts = (page, favouriteToggle) => async (dispatch) => {
@@ -15,7 +99,7 @@ export const getProducts = (page, favouriteToggle) => async (dispatch) => {
                     ? error.response.data.message
                     : error.message
                         ? error.message
-                        : 'An expected error has occured.'
+                        : 'An expected error has occured. Please try again later.'
             )
         );
     }
@@ -25,6 +109,7 @@ export const addToFavorites = (id) => async (dispatch, getState) => {
     const {
         product: { favorites },
     } = getState();
+
     const newFavorites = [...favorites, id];
     localStorage.setItem('favorites', JSON.stringify(newFavorites));
     dispatch(setFavorites(newFavorites));
@@ -34,6 +119,7 @@ export const removeFromFavorites = (id) => async (dispatch, getState) => {
     const {
         product: { favorites },
     } = getState();
+
     const newFavorites = favorites.filter((favoriteId) => favoriteId !== id);
     localStorage.setItem('favorites', JSON.stringify(newFavorites));
     dispatch(setFavorites(newFavorites));
@@ -43,9 +129,10 @@ export const toggleFavorites = (toggle) => async (dispatch, getState) => {
     const {
         product: { favorites, products },
     } = getState();
+
     if (toggle) {
         const filteredProducts = products.filter((product) => favorites.includes(product._id));
-        dispatch(setFavoritesToggle(true));
+        dispatch(setFavoritesToggle(toggle));
         dispatch(setProducts(filteredProducts));
     } else {
         dispatch(setFavoritesToggle(false));
@@ -54,9 +141,9 @@ export const toggleFavorites = (toggle) => async (dispatch, getState) => {
 };
 
 export const getProduct = (id) => async (dispatch) => {
-    dispatch(setLoading(true))
+    dispatch(setLoading(true));
     try {
-        const { data } = await axios.get(`/api/products/${id}`)
+        const { data } = await axios.get(`/api/products/${id}`);
         dispatch(setProduct(data));
     } catch (error) {
         dispatch(
@@ -65,8 +152,34 @@ export const getProduct = (id) => async (dispatch) => {
                     ? error.response.data.message
                     : error.message
                         ? error.message
-                        : 'An expected error has occured.'
+                        : 'An expected error has occured. Please try again later.'
             )
         );
     }
-}
+};
+
+export const createProductReview = (productId, userId, comment, rating, title) => async (dispatch, getState) => {
+    const {
+        user: { userInfo },
+    } = getState();
+    try {
+        const config = { headers: { Authorization: `Bearer ${userInfo.token}`, 'Content-Type': 'application/json' } };
+
+        await axios.post(`/api/products/reviews/${productId}`, { comment, userId, rating, title }, config);
+        dispatch(productReviewed(true));
+    } catch (error) {
+        dispatch(
+            setError(
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message
+                        ? error.message
+                        : 'An expected error has occured. Please try again later.'
+            )
+        );
+    }
+};
+
+export const resetProductError = () => async (dispatch) => {
+    dispatch(resetError());
+};
--- client/src/redux/slices/product.js
+++ client/src/redux/slices/product.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+
 export const initialState = {
     loading: false,
     error: null,
@@ -8,9 +9,11 @@ export const initialState = {
     favoritesToggled: false,
     reviewed: false,
     favorites: JSON.parse(localStorage.getItem('favorites')) ?? [],
+    reviewRemoval: false,
+    productUpdate: false,
 };
 
-export const productSlice = createSlice({
+export const productsSlice = createSlice({
     name: 'products',
     initialState,
     reducers: {
@@ -21,7 +24,7 @@ export const productSlice = createSlice({
             state.loading = false;
             state.error = null;
             state.products = payload;
-
+            state.reviewRemoval = false;
         },
         setProduct: (state, { payload }) => {
             state.product = payload;
@@ -43,14 +46,44 @@ export const productSlice = createSlice({
         },
         setFavoritesToggle: (state, { payload }) => {
             state.favoritesToggled = payload;
-        }
+        },
+        productReviewed: (state, { payload }) => {
+            state.loading = false;
+            state.error = null;
+            state.reviewed = payload;
+        },
+        resetError: (state) => {
+            state.error = null;
+            state.reviewed = false;
+            state.productUpdate = false;
+            state.reviewRemoval = false;
+        },
+        setProductUpdateFlag: (state) => {
+            state.productUpdate = true;
+            state.loading = false;
+        },
+        setReviewRemovalFlag: (state) => {
+            state.error = null;
+            state.reviewRemoval = true;
+            state.loading = false;
+        },
     },
 });
 
 export const {
-    setLoading, setError, setPagination, setProducts, setProduct, setFavorites, setFavoritesToggle
-} = productSlice.actions;
+    setLoading,
+    setError,
+    setProducts,
+    setPagination,
+    setFavoritesToggle,
+    setFavorites,
+    setProduct,
+    productReviewed,
+    setProductUpdateFlag,
+    resetError,
+    setReviewRemovalFlag,
+} = productsSlice.actions;
 
-export default productSlice.reducer;
+export default productsSlice.reducer;
 
 export const productSelector = (state) => state.products;
